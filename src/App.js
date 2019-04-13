6188099ee8a0e21e07b57a693a9951ef3bed4ad2
Furtive Pygmy <rzvgib001@gmail.com>
2019-04-13 16:51:56 +0400

    Setting up rank

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <p className="f3">{'Rank'}</p>
+    </div>
+  );
+};
+
+export default Rank;

74a47afebcabb6dd29fc8a09f2262367198567dc
Furtive Pygmy <rzvgib001@gmail.com>
2019-04-28 10:02:38 +0400

    Setting up password encryption

--- server.js
+++ server.js
@@ -2,15 +2,84 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// Require in API routes here
-
 const app = express();
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'john',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
+// API routes
+
+app.get('/api', (req, res) => {
+  res.json(database.users);
+});
+
+app.post('/api/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
+  } else {
+    res.status(400).json('failed');
+  }
+});
+
+app.post('/api/register', (req, res) => {
+  const { email, password, name } = req.body;
+  database.users.push({
+    id: '125',
+    name,
+    email,
+    password,
+    entries: 0,
+    joined: new Date()
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/api/profile/:id', (req, res) => {
+  const { id } = req.params;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      return res.json(user);
+    }
+  });
+  return res.status(404).json('User not found');
+});
+
+app.put('/api/image', (req, res) => {
+  const { id } = req.body;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  return res.status(404).json('User not found');
+});
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
